8cb9d3ef2e99abb57d2085288201b5fff2893362
Sofya Kopylova <0707sonya@gmail.com>
2021-05-28 18:09:17 +0300

    Create FirstViewController.

---
+++ Testpro.1/FirctViewCotroller.swift
@@ -0,0 +1,15 @@
+//
+//  FirctViewCotroller.swift
+//  Testpro.1
+//
+//  Created by Софья Копылова on 28.05.2021.
+//
+
+import UIKit
+
+class FirstViewCotroller: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
